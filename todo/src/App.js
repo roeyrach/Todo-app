dfd11bd320348447f0c40d2fb85237e6c369cb25
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-01-10 14:48:20 +0200

    almost finsihed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import TodoList from "./components/TodoList"
+import "./App.css"
 
 const todosList = []
 
 const App = () => {
+	const [darkModeEnabled, setDarkModeEnabled] = useState(false)
+
+	const toggleDarkMode = () => {
+		setDarkModeEnabled(!darkModeEnabled)
+	}
+
+	const className = darkModeEnabled ? "dark-mode" : "light-mode"
+
 	return (
 		<>
 			<div
-				id="appRoot"
+				className={className}
 				style={{
-					alignContent: "center",
-					alignItems: "center",
-					display: "flex",
-					flexDirection: "column",
-					
-					
+					height: "100vh",
 				}}
 			>
-				<div style={{padding: "15px",marginTop:"5vh", border: "1px solid black", borderRadius: "10px"}}>
-					<TodoList todos={todosList}></TodoList>
+				<div
+					id="appRoot"
+					style={{
+						alignContent: "center",
+						alignItems: "center",
+						display: "flex",
+						flexDirection: "column",
+					}}
+				>
+					<h1>TODO</h1>
+					<button
+						style={{
+							backgroundColor: darkModeEnabled ? "#333333" : "#f1f1f1",
+							color: darkModeEnabled ? "#f1f1f1" : "#333333",
+							border: `1px solid ${darkModeEnabled ? "#f1f1f1" : "#333333"}`,
+							borderRadius: "5px",
+						}}
+						onClick={toggleDarkMode}
+					>
+						Dark/Light
+					</button>
+					<div
+						style={{
+							padding: "15px",
+							marginTop: "5vh",
+							borderRadius: "10px",
+							maxWidth: "60vw",
+							backgroundColor: `${darkModeEnabled ? "#333333" : "#d8d8d8"}`,
+						}}
+					>
+						<TodoList darkModeEnabled={darkModeEnabled} todos={todosList}></TodoList>
+					</div>
 				</div>
-				
 			</div>
 		</>
 	)
--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react"
 
 function TodoItem(props) {
-	const [checked, setChecked] = useState(false)
 	return (
-		<div style={{ display: "flex" }}>
+		<div
+			style={{
+				display: "flex",
+				border: `1px solid ${props.darkModeEnabled ? "#f1f1f1" : "#333333"}`,
+				marginBottom: "1.5vh",
+				borderRadius: "5px",
+			}}
+		>
 			<label>
 				<input
-					style={{ borderRadius: "5px" }}
+					style={{
+						borderRadius: "5px",
+					}}
 					type="checkbox"
 					defaultChecked={props.cb}
 					onChange={() => {
@@ -24,12 +32,18 @@ function TodoItem(props) {
 			</label>
 
 			<button
-				style={{ marginLeft: "auto" }}
+				style={{
+					marginLeft: "auto",
+					backgroundColor: props.darkModeEnabled ? "#333333" : "#f1f1f1",
+					color: props.darkModeEnabled ? "#f1f1f1" : "#333333",
+					border: `1px solid ${props.darkModeEnabled ? "#f1f1f1" : "#333333"}`,
+					borderRadius: "5px",
+				}}
 				onClick={() => {
 					props.onClick(props.id)
 				}}
 			>
-				Delete
+				x
 			</button>
 		</div>
 	)
